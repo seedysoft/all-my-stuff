0fcf7a61d9312d0fb458e264e199728de02340c6
Seedysoft <seedysoft@gmail.com>
2023-10-17 11:57:48 +0200

    Fix logs location and compact HomeCloud.Server Program.cs

--- src/HomeCloud/Server/Program.cs
+++ src/HomeCloud/Server/Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Serilog;
+﻿using Seedysoft.HomeCloud.Server.Extensions;
 
 namespace Seedysoft.HomeCloud.Server;
 
@@ -14,11 +12,7 @@ public class Program
 
         // Add services to the container.
         if (System.Diagnostics.Debugger.IsAttached)
-        {
             _ = builder.Configuration.SetBasePath(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)!);
-
-            _ = builder.Logging.AddConsole();
-        }
 
         _ = builder.Logging.AddSystemdConsole();
 
@@ -29,132 +23,12 @@ public class Program
         // Systemd Service must be configured as type=notify
         _ = builder.Host.UseSystemd();
 
-        if (System.Diagnostics.Debugger.IsAttached)
-            builder.Environment.EnvironmentName = Environments.Development/*.Production*/;
-
-        string CurrentEnvironmentName = builder.Environment.EnvironmentName;
-        _ = builder.Configuration
-            .AddJsonFile($"appsettings.HomeCloudServer.json", false, true)
-            .AddJsonFile($"appsettings.HomeCloudServer.{CurrentEnvironmentName}.json", false, true)
-
-            .AddJsonFile($"appsettings.CarburantesConnectionStrings.{CurrentEnvironmentName}.json", false, true)
-
-            .AddJsonFile($"appsettings.Carburantes.Infrastructure.json", false, true)
-            .AddJsonFile($"appsettings.Carburantes.Infrastructure.{CurrentEnvironmentName}.json", false, true)
-
-            .AddJsonFile($"appsettings.dbConnectionString.{CurrentEnvironmentName}.json", false, true)
-
-            .AddJsonFile($"appsettings.Serilog.json", false, true)
-            .AddJsonFile($"appsettings.Serilog.{CurrentEnvironmentName}.json", false, true)
-
-            .AddJsonFile($"appsettings.ObtainPvpcSettings.json", false, true)
-            .AddJsonFile($"appsettings.SmtpServiceSettings.json", false, true)
-            .AddJsonFile($"appsettings.TelegramSettings.{CurrentEnvironmentName}.json", false, true)
-            .AddJsonFile($"appsettings.WebComparerSettings.{CurrentEnvironmentName}.json", false, true);
-
-        _ = builder.Services.AddLogging(iLoggingBuilder =>
-        {
-            IConfigurationSection configurationSection = builder.Configuration.GetRequiredSection("Serilog:WriteTo:1:Args:path");
-            Console.WriteLine("Obtained '{0}' from Serilog:WriteTo:1:Args:path", configurationSection.Value);
-
-            if (System.Diagnostics.Debugger.IsAttached)
-                configurationSection.Value = $"../../../{configurationSection.Value!}";
-
-            configurationSection.Value = Path.GetFullPath(configurationSection.Value!.Replace("{ApplicationName}", builder.Environment.ApplicationName));
-            Console.WriteLine("Final value of Serilog:WriteTo:1:Args:path: '{0}'", configurationSection.Value);
-
-            _ = iLoggingBuilder.AddSerilog(new LoggerConfiguration()
-                .ReadFrom.Configuration(builder.Configuration)
-                .CreateLogger());
-        });
-
-        _ = builder.Services
-            .AddDbContext<InfrastructureLib.DbContexts.DbCxt>((iServiceProvider, dbContextOptionsBuilder) =>
-            {
-                string ConnectionStringName = nameof(InfrastructureLib.DbContexts.DbCxt);
-                string ConnectionString = builder.Configuration.GetConnectionString($"{ConnectionStringName}") ?? throw new KeyNotFoundException($"Connection string '{ConnectionStringName}' not found.");
-                if (System.Diagnostics.Debugger.IsAttached)
-                    ConnectionString = ConnectionString.Replace($"{CoreLib.Constants.DatabaseStrings.DataSource}../../../", $"{CoreLib.Constants.DatabaseStrings.DataSource}");
-                string FullFilePath = Path.GetFullPath(ConnectionString[CoreLib.Constants.DatabaseStrings.DataSource.Length..]);
-                if (!File.Exists(FullFilePath))
-                    throw new FileNotFoundException("Database file not found: '{FullFilePath}'", FullFilePath);
-
-                _ = dbContextOptionsBuilder.UseSqlite(ConnectionString);
-                if (System.Diagnostics.Debugger.IsAttached)
-                {
-                    _ = dbContextOptionsBuilder
-                        .EnableDetailedErrors()
-                        .EnableSensitiveDataLogging()
-                        .LogTo(Console.WriteLine, LogLevel.Trace);
-                }
-            }
-            , ServiceLifetime.Transient
-            , ServiceLifetime.Transient)
-
-            .AddDbContext<Carburantes.Infrastructure.Data.CarburantesDbContext>((iServiceProvider, dbContextOptionsBuilder) =>
-            {
-                string ConnectionStringName = nameof(Carburantes.Infrastructure.Data.CarburantesDbContext);
-                string ConnectionString = builder.Configuration.GetConnectionString($"{ConnectionStringName}") ?? throw new KeyNotFoundException($"Connection string '{ConnectionStringName}' not found.");
-                if (System.Diagnostics.Debugger.IsAttached)
-                    ConnectionString = ConnectionString.Replace($"{CoreLib.Constants.DatabaseStrings.DataSource}../../../", $"{CoreLib.Constants.DatabaseStrings.DataSource}");
-                string FullFilePath = Path.GetFullPath(ConnectionString[CoreLib.Constants.DatabaseStrings.DataSource.Length..]);
-                if (!File.Exists(FullFilePath))
-                    throw new FileNotFoundException("Database file not found: '{FullFilePath}'", FullFilePath);
-
-                _ = dbContextOptionsBuilder.UseSqlite(ConnectionString);
-                if (System.Diagnostics.Debugger.IsAttached)
-                {
-                    _ = dbContextOptionsBuilder
-                        .EnableDetailedErrors()
-                        .EnableSensitiveDataLogging()
-                        .LogTo(Console.WriteLine, LogLevel.Trace);
-                }
-            }
-            , ServiceLifetime.Transient
-            , ServiceLifetime.Transient)
-
-            .AddDbContext<Carburantes.Infrastructure.Data.CarburantesHistDbContext>((iServiceProvider, dbContextOptionsBuilder) =>
-            {
-                string ConnectionStringName = nameof(Carburantes.Infrastructure.Data.CarburantesHistDbContext);
-                string ConnectionString = builder.Configuration.GetConnectionString($"{ConnectionStringName}") ?? throw new KeyNotFoundException($"Connection string '{ConnectionStringName}' not found.");
-                if (System.Diagnostics.Debugger.IsAttached)
-                    ConnectionString = ConnectionString.Replace($"{CoreLib.Constants.DatabaseStrings.DataSource}../../../", $"{CoreLib.Constants.DatabaseStrings.DataSource}");
-                string FullFilePath = Path.GetFullPath(ConnectionString[CoreLib.Constants.DatabaseStrings.DataSource.Length..]);
-                if (!File.Exists(FullFilePath))
-                    throw new FileNotFoundException("Database file not found: '{FullFilePath}'", FullFilePath);
-
-                _ = dbContextOptionsBuilder.UseSqlite(ConnectionString);
-                if (System.Diagnostics.Debugger.IsAttached)
-                {
-                    _ = dbContextOptionsBuilder
-                        .EnableDetailedErrors()
-                        .EnableSensitiveDataLogging()
-                        .LogTo(Console.WriteLine, LogLevel.Trace);
-                }
-            }
-            , ServiceLifetime.Transient
-            , ServiceLifetime.Transient);
-
-        builder.Services.TryAddSingleton(builder.Configuration.GetSection(nameof(SmtpServiceLib.Settings.SmtpServiceSettings)).Get<SmtpServiceLib.Settings.SmtpServiceSettings>()!);
-        builder.Services.TryAddScoped<SmtpServiceLib.Services.SmtpService>();
-
-        builder.Services.TryAddSingleton(builder.Configuration.GetSection(nameof(TelegramLib.Settings.TelegramSettings)).Get<TelegramLib.Settings.TelegramSettings>()!);
-        builder.Services.TryAddSingleton<TelegramLib.Services.TelegramService>();
-
-        _ = builder.Services.AddHttpClient(nameof(Carburantes.Core.Settings.Minetur));
-        _ = builder.Services.AddHostedService<Carburantes.Services.ObtainDataCronBackgroundService>();
-
-        builder.Services.TryAddSingleton(builder.Configuration.GetSection(nameof(ObtainPvpcLib.Settings.ObtainPvpcSettings)).Get<ObtainPvpcLib.Settings.ObtainPvpcSettings>()!);
-        _ = builder.Services.AddHostedService<ObtainPvpcLib.Services.ObtainPvpCronBackgroundService>();
-
-        _ = builder.Services.AddHostedService<OutboxLib.Services.OutboxCronBackgroundService>();
-
-        builder.Services.TryAddSingleton(builder.Configuration.GetSection(nameof(WebComparerLib.Settings.WebComparerSettings)).Get<WebComparerLib.Settings.WebComparerSettings>()!);
-        _ = builder.Services.AddHostedService<WebComparerLib.Services.WebComparerCronBackgroundService>();
+        // Extension method
+        _ = builder.AddMyDependencies();
 
         WebApplication webApp = builder.Build();
 
-        ILogger<Program> logger = webApp.Services.GetRequiredService<ILogger<Program>>();
+        //ILogger<Program> logger = webApp.Services.GetRequiredService<ILogger<Program>>();
 
         // Configure the HTTP request pipeline.
         if (webApp.Environment.IsDevelopment())
@@ -182,9 +56,7 @@ public class Program
         SQLitePCL.Batteries.Init();
 
         // Migrate and seed the database during startup. Must be synchronous.
-        webApp.Services.GetRequiredService<InfrastructureLib.DbContexts.DbCxt>().Database.Migrate();
-        webApp.Services.GetRequiredService<Carburantes.Infrastructure.Data.CarburantesDbContext>().Database.Migrate();
-        webApp.Services.GetRequiredService<Carburantes.Infrastructure.Data.CarburantesHistDbContext>().Database.Migrate();
+        _ = webApp.MigrateDbContexts();
 
         webApp.Run();
     }
