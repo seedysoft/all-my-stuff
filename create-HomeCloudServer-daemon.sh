731889f9f7e9eb40ec4ce828826f9f6958750582
Seedysoft <seedysoft@gmail.com>
2023-10-18 17:36:22 +0200

    Publish from raspberrypi4

--- create-HomeCloudServer-daemon.sh
+++ create-HomeCloudServer-daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. create-daemon.sh -f /mnt/syncthing/Seedysoft/linux-arm64/Seedysoft.HomeCloud.Server -s ss-homecloud
+sudo ./create-daemon.sh -f Seedysoft.HomeCloud.Server -s ss-homecloud
