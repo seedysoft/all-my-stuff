6d616bd8c2db08ba39954396c792a7a3d091d09b
Seedysoft <seedysoft@gmail.com>
2024-07-02 19:04:03 +0200

    Add project

--- test/Pvpc/Lib.Tests/PvpcBackgroundServiceCronTest.cs
+++ test/Pvpc/Lib.Tests/PvpcBackgroundServiceCronTest.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
