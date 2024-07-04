c3f7af5c8835b4307f855436f404e9127ef555f3
Seedysoft <seedysoft@gmail.com>
2024-07-04 15:43:01 +0200

    Closes #47
    Fix names

--- src/Pvpc/Lib/Extensions/PvpcLibExtensions.cs
+++ src/Pvpc/Lib/Extensions/PvpcLibExtensions.cs
@@ -2,6 +2,6 @@
 
 public static class PvpcLibExtensions
 {
-    internal static Libs.TuyaDeviceControl.TuyaDeviceBase ToTuyaDeviceBase(this Libs.Core.Entities.TuyaDevice self)
-        => new(self.Id, self.Address, self.LocalKey, version: self.Version);
+    internal static Libs.TuyaDeviceControl.TuyaDeviceBase ToTuyaDeviceBase(this Libs.Core.Entities.TuyaDevice self) =>
+        new(self.Id, self.Address, self.LocalKey, version: self.Version);
 }
