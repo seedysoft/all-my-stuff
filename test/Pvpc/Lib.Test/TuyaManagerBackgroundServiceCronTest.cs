08fdb0cc7c20bcfb97a956e3548a873bc8376217
Seedysoft <seedysoft@gmail.com>
2024-07-05 17:26:47 +0200

    Try to reference Octokit project to see where is the fail

--- test/Pvpc/Lib.Test/TuyaManagerBackgroundServiceCronTest.cs
+++ test/Pvpc/Lib.Test/TuyaManagerBackgroundServiceCronTest.cs
@@ -6,24 +6,24 @@ using Xunit;
 
 namespace Seedysoft.Pvpc.Lib.Test;
 
-public sealed class TuyaManagerCronBackgroundServiceTests(TuyaManagerCronBackgroundServiceFixture tuyaServiceFixture)
-    : IClassFixture<TuyaManagerCronBackgroundServiceFixture>
+public sealed class TuyaManagerBackgroundServiceCronTest(TuyaManagerBackgroundServiceCronFixture tuyaServiceFixture)
+    : IClassFixture<TuyaManagerBackgroundServiceCronFixture>
 {
-    public TuyaManagerCronBackgroundServiceFixture TuyaServiceFixture { get; } = tuyaServiceFixture;
+    public TuyaManagerBackgroundServiceCronFixture TuyaServiceFixture { get; } = tuyaServiceFixture;
 
     [Fact]
     public async Task DoWorkAsyncTest() => await TuyaServiceFixture.TuyaManagerService.DoWorkAsync(default);
 }
 
-public sealed class TuyaManagerCronBackgroundServiceFixture : Libs.Infrastructure.Test.BaseFixture
+public sealed class TuyaManagerBackgroundServiceCronFixture : Libs.Infrastructure.Test.BaseFixture
 {
-    public TuyaManagerCronBackgroundServiceFixture()
+    public TuyaManagerBackgroundServiceCronFixture()
     {
         IServiceCollection serviceCollection = new ServiceCollection();
         serviceCollection.TryAddSingleton(new Settings.TuyaManagerSettings() { CronExpression = "* * 30 2 *", /* At every minute on day-of-month 30 in February. */         });
         serviceCollection.TryAddSingleton(GetDbCxt());
-        serviceCollection.TryAddSingleton(new NullLogger<Services.TuyaManagerBackgroundServiceCron>());
-        serviceCollection.TryAddSingleton(new ApplicationLifetime(new NullLogger<ApplicationLifetime>()));
+        serviceCollection.TryAddSingleton<Microsoft.Extensions.Logging.ILogger<Services.TuyaManagerBackgroundServiceCron>>(new NullLogger<Services.TuyaManagerBackgroundServiceCron>());
+        serviceCollection.TryAddSingleton<Microsoft.Extensions.Hosting.IHostApplicationLifetime>(new ApplicationLifetime(new NullLogger<ApplicationLifetime>()));
 
         TuyaManagerService = new(serviceCollection.BuildServiceProvider());
     }
