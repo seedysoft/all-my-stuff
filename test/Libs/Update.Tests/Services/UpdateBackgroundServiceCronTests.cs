ccee9496ea2b4e1ca1b9a834a770b1a43016c204
Seedysoft <seedysoft@gmail.com>
2024-07-10 10:28:45 +0200

    Init DoWorkAsyncTest

--- test/Libs/Update.Tests/Services/UpdateBackgroundServiceCronTests.cs
+++ test/Libs/Update.Tests/Services/UpdateBackgroundServiceCronTests.cs
@@ -3,23 +3,27 @@ using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Seedysoft.Libs.Update.Tests;
+namespace Seedysoft.Libs.Update.Tests.Services;
 
 [TestClass]
 public sealed class UpdateBackgroundServiceCronTest : Infrastructure.Tests.TestClassBase
 {
-    private static Services.UpdateBackgroundServiceCron UpdateService = default!;
+    private static Update.Services.UpdateBackgroundServiceCron UpdateService = default!;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+
+    }
 
     [ClassInitialize]
     public static new void ClassInitialize(TestContext testContext)
     {
-        Microsoft.Extensions.Hosting.HostApplicationBuilder hostApplicationBuilder =
-            Microsoft.Extensions.Hosting.Host.CreateEmptyApplicationBuilder(null);
+        Microsoft.Extensions.Hosting.HostApplicationBuilder hostApplicationBuilder = new();
 
         new Dependencies.Configurator().AddDependencies(hostApplicationBuilder);
 
-        hostApplicationBuilder.Services.TryAddSingleton(GetDbCxt());
-        hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Logging.ILogger<Services.UpdateBackgroundServiceCron>>(new NullLogger<Services.UpdateBackgroundServiceCron>());
+        hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Logging.ILogger<Update.Services.UpdateBackgroundServiceCron>>(new NullLogger<Update.Services.UpdateBackgroundServiceCron>());
         hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Hosting.IHostApplicationLifetime>(new ApplicationLifetime(new NullLogger<ApplicationLifetime>()));
 
         Microsoft.Extensions.Hosting.IHost host = hostApplicationBuilder.Build();
@@ -32,4 +36,7 @@ public sealed class UpdateBackgroundServiceCronTest : Infrastructure.Tests.TestC
 
     [TestMethod]
     public async Task IsConnectionTest() => Assert.IsTrue(await UpdateService.ConnectAsync());
+
+    [TestMethod]
+    public async Task DoWorkAsyncTest() => await UpdateService.DoWorkAsync(CancellationToken.None);
 }
