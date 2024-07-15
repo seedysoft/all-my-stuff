fb38f51866cc9b3db3d8326124a3f0e6f356aa61
Seedysoft <seedysoft@gmail.com>
2024-07-15 11:55:03 +0200

    Move Update to dedicate folder

--- test/Update/Lib.Tests/Services/UpdateBackgroundServiceCronTests.cs
+++ test/Update/Lib.Tests/Services/UpdateBackgroundServiceCronTests.cs
@@ -1,14 +1,16 @@
+using Seedysoft.Libs.Utils.Dependencies;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting.Internal;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Seedysoft.Update.Lib.Services;
 
-namespace Seedysoft.Libs.Update.Tests.Services;
+namespace Seedysoft.Update.Lib.Tests.Services;
 
 [TestClass]
-public sealed class UpdateBackgroundServiceCronTest : Infrastructure.Tests.TestClassBase
+public sealed class UpdateBackgroundServiceCronTest : Libs.Infrastructure.Tests.TestClassBase
 {
-    private static Update.Services.UpdateBackgroundServiceCron UpdateService = default!;
+    private static UpdateBackgroundServiceCron UpdateService = default!;
 
     [TestInitialize]
     public void TestInitialize()
@@ -23,7 +25,7 @@ public sealed class UpdateBackgroundServiceCronTest : Infrastructure.Tests.TestC
 
         new Dependencies.Configurator().AddDependencies(hostApplicationBuilder);
 
-        hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Logging.ILogger<Update.Services.UpdateBackgroundServiceCron>>(new NullLogger<Update.Services.UpdateBackgroundServiceCron>());
+        hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Logging.ILogger<UpdateBackgroundServiceCron>>(new NullLogger<UpdateBackgroundServiceCron>());
         hostApplicationBuilder.Services.TryAddSingleton<Microsoft.Extensions.Hosting.IHostApplicationLifetime>(new ApplicationLifetime(new NullLogger<ApplicationLifetime>()));
 
         Microsoft.Extensions.Hosting.IHost host = hostApplicationBuilder.Build();
@@ -48,7 +50,7 @@ public sealed class UpdateBackgroundServiceCronTest : Infrastructure.Tests.TestC
         string sourceFileName = Path.Combine(tempDir, "win-x64.v1.1.1.6.7z");
 
         if (File.Exists(sourceFileName))
-            UpdateService.ExtractFile(extractorFileName, sourceFileName, tempDir);
+            Assert.IsTrue(UpdateService.ExtractFile(extractorFileName, sourceFileName, tempDir));
         else
             Assert.Inconclusive();
     }
