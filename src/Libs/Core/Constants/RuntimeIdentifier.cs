fd15c38c1448f5f82cf42502a4e54166dd444264
Seedysoft <seedysoft@gmail.com>
2024-07-18 11:09:50 +0200

    Use RuntimeIdentifier we use

---
+++ src/Libs/Core/Constants/RuntimeIdentifier.cs
@@ -0,0 +1,8 @@
+﻿namespace Seedysoft.Libs.Core.Constants;
+
+public static class RuntimeIdentifier
+{
+    public const string LinuxArm64 = "linux-arm64";
+    public const string LinuxX64 = "linux-x64";
+    public const string WinX64 = "win-x64";
+}
