6b20935144498c44cc5e8a06b2a36d2d35684d53
SeedySoft <seedysoft@gmail.com>
2024-11-10 17:43:31 +0100

    Refined Map and Google calls

--- src/Libs/Core/Constants/Earth.cs
+++ src/Libs/Core/Constants/Earth.cs
@@ -19,13 +19,13 @@ public static class Earth
     /// </summary>
     public const double MinLongitudeInDegrees = -MaxLongitudeInDegrees;
 
+    //public const double MeanRadiusInMeters = 6_371_008.8d;
+
+    //public const int Degrees = 360;
+
     public readonly record struct Home
     {
         public const double Lat = 42.354397413084406d;
         public const double Lng = -3.6628374207940815d;
     }
-
-    public const double RadiusMeanInMeters = 6_371_008.8d;
-
-    public const int Degrees = 360;
 }
