01d330d756e09d46e8af39102f74f3f833a6fc9b
SeedySoft <seedysoft@gmail.com>
2025-01-01 22:21:55 +0100

    Use scripts to update

--- src/Libs/Update/Services/UpdaterCronBackgroundService.cs
+++ src/Libs/Update/Services/UpdaterCronBackgroundService.cs
@@ -7,19 +7,13 @@ namespace Seedysoft.Libs.Update.Services;
 
 public sealed class UpdaterCronBackgroundService : BackgroundServices.Cron
 {
-    private readonly Octokit.GitHubClient GitHubClient;
+    private readonly Octokit.GitHubClient gitHubClient;
     private readonly ILogger<UpdaterCronBackgroundService> Logger;
 
     public UpdaterCronBackgroundService(IServiceProvider serviceProvider, Microsoft.Extensions.Hosting.IHostApplicationLifetime hostApplicationLifetime)
         : base(serviceProvider, hostApplicationLifetime)
     {
-        GitHubClient = serviceProvider.GetRequiredService<Octokit.GitHubClient>();
-        //string GitHubToken = "--- token goes here ---";
-        //var tokenAuth = new Octokit.Credentials(GitHubToken);
-        //client.Credentials = tokenAuth;
-
-        Config = ServiceProvider.GetRequiredService<IConfiguration>().GetSection(nameof(Settings.UpdateSettings)).Get<Settings.UpdateSettings>()!;
-
+        gitHubClient = ServiceProvider.GetRequiredService<Octokit.GitHubClient>();
         Logger = ServiceProvider.GetRequiredService<ILogger<UpdaterCronBackgroundService>>();
     }
 
@@ -34,9 +28,11 @@ public sealed class UpdaterCronBackgroundService : BackgroundServices.Cron
 
         try
         {
-            Enums.UpdateResults UpgradeResult = await CheckAndUpgradeToNewVersion();
-
-            Logger.LogInformation($"Updating result: {UpgradeResult}");
+            if (await IsNewVersionAvailableAsync())
+            {
+                // Execute external process
+                ExecuteUpdateScript();
+            }
         }
         catch (Exception e) when (Logger.LogAndHandle(e, "Unexpected error")) { }
         finally { await Task.CompletedTask; }
@@ -44,58 +40,7 @@ public sealed class UpdaterCronBackgroundService : BackgroundServices.Cron
         Logger.LogInformation("End {ApplicationName}", AppName);
     }
 
-    internal async Task<Enums.UpdateResults> CheckAndUpgradeToNewVersion()
-    {
-        Octokit.Release? release = await GetLatestReleaseFromGithubAsync();
-        if (release == null)
-        {
-            Logger.LogError("LatestReleaseFromGithub is null.");
-            return Enums.UpdateResults.LatestReleaseFromGithubIsNull;
-        }
-
-        var ExecutingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-        Version? CurrentVersion = ExecutingAssembly.GetName().Version;
-        Version? NewVersion = new(release.Name);
-
-        if (NewVersion <= CurrentVersion)
-        {
-            Logger.LogInformation($"Current version is: {CurrentVersion}. Latest version is: {NewVersion}");
-            return Enums.UpdateResults.NoNewVersionFound;
-        }
-
-        Octokit.ReleaseAsset? asset =
-            release.Assets.FirstOrDefault(x => x.Name.Contains(System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier, StringComparison.InvariantCultureIgnoreCase));
-        if (asset == null)
-        {
-            Logger.LogError($"Asset not found for {System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier}.");
-            return Enums.UpdateResults.AssetNotFound;
-        }
-
-        if (!ExecuteUpdateScript(new Uri(asset.BrowserDownloadUrl)))
-        {
-            Logger.LogError("Cannot execute update script");
-            return Enums.UpdateResults.ErrorExecutingUpdateScript;
-        }
-
-        Logger.LogInformation("Update in progress...");
-
-        Environment.Exit(0);
-
-        return Enums.UpdateResults.Ok;
-    }
-
-    internal async Task<Octokit.Release?> GetLatestReleaseFromGithubAsync()
-    {
-        // Retrieve a List of Releases in the Repository, and get latest using [0]-subscript
-        IReadOnlyList<Octokit.Release> releases =
-            await GitHubClient.Repository.Release.GetAll(Core.Constants.Github.OwnerName, Core.Constants.Github.RepositoryName);
-
-        Logger.LogInformation("Obtained {releasesCount} releases", releases.Count);
-
-        return releases.Any() ? releases[0] : null;
-    }
-
-    internal bool ExecuteUpdateScript(Uri assetUri)
+    private void ExecuteUpdateScript()
     {
         System.Diagnostics.ProcessStartInfo processStartInfo = new()
         {
@@ -106,21 +51,82 @@ public sealed class UpdaterCronBackgroundService : BackgroundServices.Cron
         string runtimeIdentifier = System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier;
         switch (runtimeIdentifier)
         {
-            case Core.Constants.SupportedRuntimeIdentifiers.LinuxArm64:
-                //case Core.Constants.SupportedRuntimeIdentifiers.LinuxX64:
-                processStartInfo.FileName = $"sudo ./update.sh {assetUri}";
+            case "linux-arm64":
+            case "linux-x64":
+                processStartInfo.FileName = "update.sh";
                 break;
 
-            //case Core.Constants.SupportedRuntimeIdentifiers.WinX64:
-            //    processStartInfo.FileName = $"update.bat {zipFileName}";
-            //    break;
+            case "win-x64":
+                processStartInfo.FileName = "update.bat";
+                break;
 
             default:
-                // TODO: use interpolated strings in all solution
                 Logger.LogError("RuntimeIdentifier {runtimeIdentifier} not supported", runtimeIdentifier);
-                return false;
+                break;
         }
 
-        return System.Diagnostics.Process.Start(processStartInfo) != null;
+        _ = System.Diagnostics.Process.Start(processStartInfo);
+    }
+
+    private async Task<bool> IsNewVersionAvailableAsync()
+    {
+        Octokit.Release? latestRelease = await GetLatestReleaseFromGithubAsync();
+
+        if (latestRelease == null)
+            return false;
+
+        // System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+        // System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+        // string version = fvi.FileVersion;
+
+        Version? CurrentVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        Version? LatestVersion = new(latestRelease.Name);
+
+        Logger.LogInformation("Current version is: {CurrentVersion}", CurrentVersion);
+        Logger.LogInformation("Latest version is: {LatestVersion}", LatestVersion);
+
+        return LatestVersion > CurrentVersion && !string.IsNullOrEmpty(await DownloadReleaseFromGithubAsync(latestRelease));
+    }
+
+    internal async Task<Octokit.Release?> GetLatestReleaseFromGithubAsync()
+    {
+        IReadOnlyList<Octokit.Release> releases =
+            await gitHubClient.Repository.Release.GetAll(Core.Constants.Github.OwnerName, Core.Constants.Github.RepositoryName);
+
+        Logger.LogInformation("Obtained {releasesCount} releases", releases.Count);
+
+        return releases.Any() ? releases[0] : null;
+    }
+
+    internal async Task<string> DownloadReleaseFromGithubAsync(Octokit.Release release)
+    {
+        foreach (Octokit.ReleaseAsset? asset in release.Assets)
+        {
+            if (!asset.Name.Contains($"{System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier}", StringComparison.InvariantCultureIgnoreCase))
+                continue;
+
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/octet-stream");
+            //httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Core.Constants.Github.Token);
+
+            Logger.LogInformation("Try to download {assetBrowserDownloadUrl}", asset.BrowserDownloadUrl);
+
+            HttpResponseMessage response = await httpClient.GetAsync(asset.BrowserDownloadUrl);
+
+            _ = response.EnsureSuccessStatusCode();
+
+            using (Stream stream = await response.Content.ReadAsStreamAsync())
+            {
+                using FileStream fileStream = File.Create(asset.Name);
+                await stream.CopyToAsync(fileStream);
+            }
+
+            Logger.LogInformation("Downloaded {assetName}", asset.Name);
+
+            return asset.Name;
+        }
+
+        return string.Empty;
     }
 }
