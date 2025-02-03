1c79b867037cfc35351077264e2fdfea7e34548b
seedysoft <seedysoft@gmail.com>
2025-02-03 20:18:22 +0100

    Try to fix gas stations datagrid

--- src/BlazorWebApp/Client/Components/GasStationsViewer.razor.cs
+++ src/BlazorWebApp/Client/Components/GasStationsViewer.razor.cs
@@ -1,40 +1,51 @@
 using Microsoft.AspNetCore.Components;
 
+using Model = Seedysoft.Libs.GasStationPrices.ViewModels.GasStationModel;
+
 namespace Seedysoft.BlazorWebApp.Client.Components;
 
 public partial class GasStationsViewer
 {
-    [Parameter] public IEnumerable<Libs.GasStationPrices.ViewModels.GasStationModel> GasStationItems { get; set; } = [];
+    #region Parameters
+
+    [Parameter] public IEnumerable<Model> Elements { get; set; } = [];
 
     [Parameter] public bool IsLoading { get; set; }
 
-    [Parameter] public Func<Libs.GasStationPrices.ViewModels.GasStationModel, Task>? OnSelectedItemChanged { get; set; }
+    [Parameter] public EventCallback<Model> OnSelectedItemChanged { get; set; } = default!;
 
     [Parameter] public IReadOnlyCollection<long> PetroleumProductsSelectedIds { get; set; } = [];
 
-    private MudBlazor.MudDataGrid<Libs.GasStationPrices.ViewModels.GasStationModel> DataGridRef { get; set; } = default!;
+    #endregion
 
-    private bool IsRotuloFilterOpen = false;
-    private MudBlazor.FilterDefinition<Libs.GasStationPrices.ViewModels.GasStationModel> RotuloFilterDefinition = default!;
-    private HashSet<string> RotuloFilterAvailableItems => [.. GasStationItems.Select(static x => x.RotuloTrimed).Distinct().Order()];
-    private HashSet<string> RotuloFilterSelectedItems = [];
-    private bool IsRotuloFilterAllSelected => RotuloFilterAvailableItems.Count == RotuloFilterSelectedItems.Count;
-    public string RotuloFilterIcon => IsRotuloFilterAllSelected switch
-    {
-        true => MudBlazor.Icons.Material.Outlined.FilterAlt,
-        false => MudBlazor.Icons.Material.Filled.FilterAlt,
-    };
+    private MudBlazor.MudDataGrid<Model> DataGridRef { get; set; } = default!;
 
     protected override Task OnInitializedAsync()
     {
         RotuloFilterSelectedItems = RotuloFilterAvailableItems;
-        RotuloFilterDefinition = new MudBlazor.FilterDefinition<Libs.GasStationPrices.ViewModels.GasStationModel>()
+        RotuloFilterDefinition = new MudBlazor.FilterDefinition<Model>()
         {
             FilterFunction = x => RotuloFilterSelectedItems.Contains(x.RotuloTrimed)
         };
 
         return base.OnInitializedAsync();
     }
+
+    private string RowClassFuncion(Model element, int rowNumber)
+        => element.Equals(DataGridRef.SelectedItem) ? "cursor-pointer mud-primary" : "cursor-pointer";
+
+    #region RotuloFiltering
+
+    private bool IsRotuloFilterOpen = false;
+    private MudBlazor.FilterDefinition<Model> RotuloFilterDefinition = default!;
+    private SortedSet<string> RotuloFilterAvailableItems => [.. Elements.Select(static x => x.RotuloTrimed).Distinct()];
+    private SortedSet<string> RotuloFilterSelectedItems = [];
+    private bool IsRotuloFilterAllSelected => RotuloFilterAvailableItems.Count == RotuloFilterSelectedItems.Count;
+    public string RotuloFilterIcon => IsRotuloFilterAllSelected switch
+    {
+        true => MudBlazor.Icons.Material.Outlined.FilterList,
+        false => MudBlazor.Icons.Material.Filled.FilterList,
+    };
 
     private void RotuloFilterSelectAll(bool value)
     {
@@ -43,38 +54,26 @@ public partial class GasStationsViewer
         else
             RotuloFilterSelectedItems.Clear();
     }
+
     private void RotuloFilterSelectedChanged(bool value, string item) =>
-        _ = value ? RotuloFilterSelectedItems.Add(item) : RotuloFilterSelectedItems.Remove(item);
-    private async Task RotuloFilterClearAsync(
-        MudBlazor.FilterContext<Libs.GasStationPrices.ViewModels.GasStationModel> gasStationModel)
+       _ = value ? RotuloFilterSelectedItems.Add(item) : RotuloFilterSelectedItems.Remove(item);
+
+    private async Task RotuloFilterClearAsync(MudBlazor.FilterContext<Model> gasStationModel)
     {
+        IsLoading = true;
         RotuloFilterSelectedItems = [.. RotuloFilterAvailableItems];
         await gasStationModel.Actions.ClearFilterAsync(RotuloFilterDefinition);
         IsRotuloFilterOpen = false;
+        IsLoading = false;
     }
-    private async Task RotuloFilterApplyAsync(MudBlazor.FilterContext<Libs.GasStationPrices.ViewModels.GasStationModel> gasStationModel)
+
+    private async Task RotuloFilterApplyAsync(MudBlazor.FilterContext<Model> gasStationModel)
     {
+        IsLoading = true;
         await gasStationModel.Actions.ApplyFilterAsync(RotuloFilterDefinition);
         IsRotuloFilterOpen = false;
+        IsLoading = false;
     }
 
-    private async Task OnRowClickGasStationsMudTable(
-        MudBlazor.TableRowClickEventArgs<Libs.GasStationPrices.ViewModels.GasStationModel> tableRowClickEventArgs)
-    {
-        if (OnSelectedItemChanged != null && tableRowClickEventArgs.Item != null)
-            await OnSelectedItemChanged.Invoke(tableRowClickEventArgs.Item);
-    }
-
-    private string SelectedRowClassFunc(
-        Libs.GasStationPrices.ViewModels.GasStationModel element,
-        int rowNumber)
-    {
-        return (DataGridRef.SelectedItem != null && DataGridRef.SelectedItem.Equals(element))
-            ? "selected"
-            : string.Empty;
-    }
-
-    //private void PaginationSelectedChanged(int i) => DataGridRef.NavigateTo(i - 1);
-
-    private async Task ClearDataAsync() => RotuloFilterSelectedItems.Clear();
+    #endregion
 }
--- src/Libs/GasStationPrices/ViewModels/ProductPrice.cs
+++ src/Libs/GasStationPrices/ViewModels/ProductPrice.cs
@@ -1,3 +1,4 @@
 ﻿namespace Seedysoft.Libs.GasStationPrices.ViewModels;
 
-public record class ProductPrice(Models.Minetur.ProductoPetrolifero Product, decimal? Price) { }
+//public record class ProductPrice(Models.Minetur.ProductoPetrolifero Product, decimal? Price) { }
+public record class ProductPrice(long ProductId, decimal? Price) { }
