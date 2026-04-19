86a41b3657bac039eaa15fe712a6fcf25a9b9c85
SeedySoft <seedysoft@gmail.com>
2026-04-19 20:12:08 +0200

    Remove Controllers
    Deleted ApiControllerBase, PetroleumProductsController, and TravelController, case all code was commented.

--- src/BlazorWebApp/Server/Controllers/ApiControllerBase.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Seedysoft.BlazorWebApp.Server.Controllers;
-
-//[Microsoft.AspNetCore.Mvc.ApiController]
-//public abstract class ApiControllerBase(ILogger logger) : Microsoft.AspNetCore.Mvc.ControllerBase
-//{
-//    protected virtual ILogger Logger { get; init; } = logger;
-
-//    //protected internal System.Text.Json.JsonSerializerOptions JsonOptions { get; } = new() { };
-//}
