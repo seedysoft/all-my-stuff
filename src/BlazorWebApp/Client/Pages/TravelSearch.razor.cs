14e67251ae1341ab59c31c2618c269c6ab896c44
seedysoft <seedysoft@gmail.com>
2026-06-05 22:36:00 +0200

    Add MapRazorClassLibrary with MapComponent and JS interop
    Introduced Seedysoft.Libs.MapRazorClassLibrary with a new MapComponent using LeafletForBlazor, including Razor, code-behind, and CSS files. Integrated the map into TravelSearch.razor, added JS interop example, background image, and updated project references and solution files accordingly.

--- src/BlazorWebApp/Client/Pages/TravelSearch.razor.cs
+++ src/BlazorWebApp/Client/Pages/TravelSearch.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using RestSharp;
 using Seedysoft.Libs.Core.Extensions;
 
 namespace Seedysoft.BlazorWebApp.Client.Pages;
@@ -11,19 +12,28 @@ public partial class TravelSearch
     [Inject] private ILogger<TravelSearch> Logger { get; set; } = default!;
     //[Inject] private MudBlazor.IDialogService DialogService { get; set; } = default!;
     [Inject] private MudBlazor.ISnackbar Snackbar { get; set; } = default!;
+    [Inject] private IConfiguration Configuration { get; set; } = default!;
     [Inject] private Libs.GasStationPrices.Services.GasStationPricesService GasStationPricesService { get; set; } = default!;
-    [Inject] private Libs.Geography.Services.PlacesService PlacesService { get; set; } = default!;
+    //[Inject] private Libs.GoogleApis.Services.PlacesService PlacesService { get; set; } = default!;
 
+    //private Libs.GoogleApis.Settings.GoogleApisSettings googleApisSettings = default!;
+
+    //private Libs.GoogleMapsRazorClassLib.GoogleMap.Map TravelGoogleMap { get; set; } = default!;
     private Libs.MapRazorClassLibrary.MapComponent TravelMap { get; set; } = default!;
 
-    private readonly Libs.Geography.ViewModels.TravelQueryModel travelQueryModel = Libs.Geography.ViewModels.TravelQueryModel.
+    private readonly Libs.GasStationPrices.ViewModels.TravelQueryModel travelQueryModel = new()
+    {
 #if DEBUG
-            CreateDefault()
+        Origin = "Calle Juan Ramon Jimenez, 8, Burgos, Spain",
+        Destination = "Manciles, Spain", /*"Calle la Iglesia, 11, Brazuelo, Leon, Spain",*/
 #else
-            CreateEmpty()
+        Origin = string.Empty,
+        Destination = string.Empty,
 #endif
-;
-    private readonly Libs.Geography.ViewModels.TravelQueryModelFluentValidator travelQueryModelFluentValidator = new();
+        MaxDistanceInKm = 10,
+        PetroleumProductsSelectedIds = Libs.GasStationPrices.Models.Minetur.ProductoPetrolifero.Gasoline.Select(static x => x.IdProducto).ToHashSet(),
+    };
+    private readonly Libs.GasStationPrices.ViewModels.TravelQueryModelFluentValidator travelQueryModelFluentValidator = new();
 
     private bool GasStationsViewerIsLoading;
     private readonly List<Libs.GasStationPrices.ViewModels.GasStationModel> GasStationItems = [];
@@ -34,14 +44,18 @@ public partial class TravelSearch
 
         if (Logger.IsEnabled(LogLevel.Information))
             Logger.LogInformation($"Called {nameof(OnInitializedAsync)}");
+
+        //googleApisSettings = Configuration
+        //    .GetSection(nameof(Libs.GoogleApis.Settings.GoogleApisSettings))
+        //    .Get<Libs.GoogleApis.Settings.GoogleApisSettings>()!;
     }
 
     private async Task OnGasStationSelectedItemChanged(Libs.GasStationPrices.ViewModels.GasStationModel gasStationModel)
     {
         //if (gasStationModel == null)
-        //    await TravelMap.ResetViewportAsync();
+        //    await TravelGoogleMap.ResetViewportAsync();
         //else
-        //    await TravelMap.ClickOnMarkerAsync(gasStationModel.ToMarker(travelQueryModel.PetroleumProductsSelectedIds));
+        //    await TravelGoogleMap.ClickOnMarkerAsync(gasStationModel.ToMarker(travelQueryModel.PetroleumProductsSelectedIds));
     }
 
     private async Task OnClickGmapRouteAsync(string encodedPolyline)
@@ -65,7 +79,7 @@ public partial class TravelSearch
     //    => _ = Snackbar.Add($"Clicked in {marker.Content}. DateTime: {DateTime.Now}", MudBlazor.Severity.Success);
     // OnClickGmapMarkerEventCallback="@OnClickGmapMarker"
 
-    private async Task<IEnumerable<Libs.Geography.ViewModels.Place>> FindPlacesAsync(string textToFind, CancellationToken cancellationToken)
+    private async Task<IEnumerable<string>> FindPlacesAsync(string textToFind, CancellationToken cancellationToken)
     {
         //try
         //{
@@ -79,8 +93,7 @@ public partial class TravelSearch
 
     private async Task ClearDataAsync()
     {
-        await TravelMap.ClearMapAsync();
-
+        //await TravelGoogleMap.RemoveAllMarkersAsync();
         GasStationItems.Clear();
     }
 
@@ -89,7 +102,7 @@ public partial class TravelSearch
         FluentValidation.Results.ValidationResult validationResult = await travelQueryModelFluentValidator.ValidateAsync(travelQueryModel);
         if (validationResult.IsValid)
         {
-            await LoadRoutesAsync();
+            await LoadGoogleRoutesAsync();
         }
         else
         {
@@ -98,10 +111,10 @@ public partial class TravelSearch
         }
     }
 
-    private async Task LoadRoutesAsync()
+    private async Task LoadGoogleRoutesAsync()
     {
         await ClearDataAsync();
 
-        await TravelMap.SearchRoutesAsync(travelQueryModel);
+        //await TravelGoogleMap.SearchRoutesAsync(travelQueryModel.Origin, travelQueryModel.Destination);
     }
 }
--- src/Libs/MapRazorClassLibrary/MapComponent.razor.cs
+++ src/Libs/MapRazorClassLibrary/MapComponent.razor.cs
@@ -5,11 +5,7 @@ namespace Seedysoft.Libs.MapRazorClassLibrary;
 
 public partial class MapComponent
 {
-    private readonly string[] Colors = ["#007FFF", "#0074EA", "#0069D5", "#005EC0", "#0053AB", "#004896", "#003D81", "#00326C"];
-
     private RealTimeMap realTimeMap = new();
-
-    [Inject] private Geography.Services.RoutesService RoutesService { get; set; } = default!;
 
     private readonly RealTimeMap.LoadParameters parameters = new()  //general map settings
     {
@@ -135,182 +131,18 @@ public partial class MapComponent
     /// </remarks>
     [Parameter] public int Zoom { get; set; } = 18;
 
-    //protected override Task OnInitializedAsync()
-    //{
-    //    return base.OnInitializedAsync();
-    //}
+    protected override Task OnInitializedAsync()
+    {
 
-    public static async Task OnAfterMapLoaded(RealTimeMap.MapEventArgs args) { }
+
+        return base.OnInitializedAsync();
+    }
 
     private void OnClickMap(RealTimeMap.ClicksMapArgs args)
     {
         if (args.sender != null && realTimeMap == args.sender)
         {
-            Console.WriteLine($"{args.location}");
+            Console.WriteLine(@"");
         }
-    }
-
-    public async Task SearchRoutesAsync(Geography.ViewModels.TravelQueryModel model)
-    {
-        await ClearMapAsync();
-
-        IList<(string NombreRuta, double[][] Coordenadas)> res = await RoutesService.GetRoutesAsync(model, CancellationToken.None);
-
-        for (int i = 0; i < res.Count; i++)
-        {
-            (string? NombreRuta, double[][]? Coordenadas) = res[i];
-
-            Geography.Models.GeoJSONLineStringClass inputPolygon = new()
-            {
-                Type = "Feature",
-                Geometry = new Geography.Models.LineStringGeometry()
-                {
-                    Type = "Polyline",
-                    Coordinates = Coordenadas,
-                },
-                Properties = new Geography.Models.Properties()
-                {
-                    Name = NombreRuta,
-                },
-            };
-
-            Geography.Models.GeoJSONPolygonAppearanceClass polygonAppearance = new()
-            {
-                Data = [inputPolygon],
-                Name = NombreRuta,
-                Symbology = new Geography.Models.PolygonSymbol()
-                {
-                    Color = Colors[i],
-                    Opacity = 0.6,
-                    Weight = 8,
-                },
-            };
-
-            await realTimeMap.Geometric.DataFromGeoJSON.addObject(polygonAppearance);
-        }
-
-        RealTimeMap.Bounds bounds = new()
-        {
-            northEast = new RealTimeMap.Location()
-            {
-                latitude = res.SelectMany(r => r.Coordenadas).Max(c => c[0]),
-                longitude = res.SelectMany(r => r.Coordenadas).Max(c => c[1]),
-            },
-            southWest = new RealTimeMap.Location()
-            {
-                latitude = res.SelectMany(r => r.Coordenadas).Min(c => c[0]),
-                longitude = res.SelectMany(r => r.Coordenadas).Min(c => c[1]),
-            },
-        };
-        realTimeMap.View.setBounds = bounds;
-
-        if (res.Count == 1)
-            SelectTrip();
-
-        //List<Geography.Models.GeoJSONItem> inputPointsList =
-        //[
-        //    new Geography.Models.GeoJSONItem()
-        //    {
-        //        Type = "Feature",
-        //        Geometry = new Geography.Models.PointGeometry()
-        //        {
-        //            Type = "Point",
-        //            Coordinates = [43.96898521116147, 25.337392340780355],
-        //        },
-        //        Properties = new Geography.Models.Properties()
-        //        {
-        //            Name = "name",
-        //        },
-        //    },
-        //    new Geography.Models.GeoJSONItem()
-        //    {
-        //        Type = "Feature",
-        //        Geometry = new Geography.Models.PointGeometry()
-        //        {
-        //            Type = "Point",
-        //            Coordinates  = [43.97596818245641, 25.33369159513244],
-
-        //        },
-        //        Properties = new Geography.Models.Properties()
-        //        {
-        //            Name = "name",
-        //        },
-        //    },
-        //];
-
-        //List<Geography.Models.GeoJSONLineStringClass> inputPolygonList =
-        //[
-        //    new Geography.Models.GeoJSONLineStringClass()
-        //    {
-        //        Type = "Feature",
-        //        Geometry = new Geography.Models.LineStringGeometry()
-        //        {
-        //            Type = "Polyline",
-        //            Coordinates =
-        //            [
-        //                [43.97209871008421, 25.32876177213506],
-        //                [43.97200458957660, 25.32911901903800],
-        //                [43.97191506364906, 25.32894358371036],
-        //                [43.97174519194365, 25.32888935676797],
-        //                [43.97186226638604, 25.32880323607917],
-        //                [43.97183242466567, 25.32865650950876],
-        //             ],
-        //        },
-        //        Properties = new Geography.Models.Properties()
-        //        {
-        //            Name = "name",
-        //        },
-        //    },
-        //];
-
-        //Geography.Models.GeoJSONPolygonAppearanceClass polygonsAppearance = new()
-        //{
-        //    Data = [.. inputPolygonList],
-        //    Name = "Polygon",
-        //    Symbology = new Geography.Models.PolygonSymbol()
-        //    {
-        //        Color = "red",
-        //        Opacity = 0.6,
-        //        Weight = 8,
-        //    },
-        //};
-
-        //Geography.Models.GeoJSONPointAppearanceClass pointsAppearance = new()
-        //{
-        //    Data = [.. inputPointsList],
-        //    Name = "points",
-        //    Symbology = new Geography.Models.PointSymbol()
-        //    {
-        //        Color = "red",
-        //        Radius = 10,
-        //    },
-        //    Tooltip = new Geography.Models.Tooltip()
-        //    {
-        //        Content = "Points",
-        //        Offset = [2, 2],
-        //        Permanent = true,
-        //        Opacity = 0.6,
-        //        VisibilityZoomLevels = new Geography.Models.VisibilityZoomLevel()
-        //        {
-        //            MaxZoomLevel = 16,
-        //            MinZoomLevel = 14,
-        //        },
-        //    }
-        //};
-
-        //await realTimeMap.Geometric.DataFromGeoJSON.addObject(polygonsAppearance);
-        //await realTimeMap.Geometric.DataFromGeoJSON.addObject(pointsAppearance);
-    }
-
-    private void SelectTrip()
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task ClearMapAsync()
-    {
-        await realTimeMap.Geometric.DataFromGeoJSON.clearMap();
-
-        //realTimeMap.Map.refresh();
     }
 }
