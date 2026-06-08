6a4491b9b678e703616f2c14a9abb15584e17d37
seedysoft <seedysoft@gmail.com>
2026-06-08 22:47:47 +0200

    Refactor GeoJSON & routing; add tests, update OSRM API
    - Refactored GeoJSON models for flexible feature/symbology support
    - Changed router API to return route name/coordinates tuples
    - Centralized coordinate inversion in RouterBase
    - Updated RoutesService and MapComponent for new data format
    - Switched OSRM API URL to HTTPS
    - Added Seedysoft.Libs.Geography.Tests project and RouterTests

--- src/Libs/Geography/Services/Routers/RouterBase.cs
+++ src/Libs/Geography/Services/Routers/RouterBase.cs
@@ -1,9 +1,11 @@
 ﻿namespace Seedysoft.Libs.Geography.Services.Routers;
 
-public abstract class RouterBase(Settings.Api api)
+internal abstract class RouterBase(Settings.Api api)
 {
     protected RestSharp.RestClient RestClient { get; } = new(new Uri(api.UrlFormat).GetLeftPart(UriPartial.Authority));
     protected Settings.Api Api { get; } = api;
 
-    public abstract Task<IList<Models.GeoJSONItem>> GetRoutesAsync(ViewModels.TravelQueryModel model, CancellationToken cancellationToken);
+    internal abstract Task<IList<(string NombreRuta, double[][] Coordenadas)>> GetRoutesAsync(ViewModels.TravelQueryModel model, CancellationToken cancellationToken);
+
+    internal static double[][] InvertCoordinates(double[][] matrix) => [.. matrix.Select(static (row, i) => new[] { row[1], row[0] })];
 }
