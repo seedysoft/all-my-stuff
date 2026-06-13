9354746d19fa9f17e41dcf4e288c83e6db033056
seedysoft <seedysoft@gmail.com>
2026-06-13 15:44:21 +0200

    Update to use fully qualified type names throughout
    Replaced ambiguous type references with fully qualified names for Settings, Models, ViewModels, and ConfiguratorBase to reduce naming conflicts. Removed unused using directives for improved code clarity.

--- src/Libs/GasStationPrices/Services/GeographyServiceBase.cs
+++ src/Libs/GasStationPrices/Services/GeographyServiceBase.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Seedysoft.Libs.GasStationPrices.Settings;
 
 namespace Seedysoft.Libs.GasStationPrices.Services;
 
 public abstract class GeographyServiceBase(IConfiguration configuration)
 {
-    protected GeographySettings GeographySettings => configuration
-        .GetSection(nameof(Settings.GeographySettings)).Get<GeographySettings>()!;
+    protected Settings.GeographySettings GeographySettings => configuration
+        .GetSection(nameof(Settings.GeographySettings)).Get<Settings.GeographySettings>()!;
 }
