c555398bb62f0c117b3fbb1e9d41939a2fb3b741
SeedySoft <seedysoft@gmail.com>
2026-08-03 18:15:04 +0200

    Refactor: migrate from RestSharp to HttpClient/DI
    Refactored all projects to remove RestSharp and Newtonsoft.Json dependencies. Adopted IHttpClientFactory and HttpClient for HTTP requests, updating service classes and constructors for DI. Introduced base classes for shared HTTP logic and switched to System.Text.Json for serialization. Updated csproj files and changed default routing to Valhalla. Improved code comments and documentation.

--- src/Libs/GasStationPrices/Dependencies/Configurator.cs
+++ src/Libs/GasStationPrices/Dependencies/Configurator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Seedysoft.Libs.GasStationPrices.Dependencies;
@@ -12,11 +13,36 @@ public sealed class Configurator : Core.Dependencies.ConfiguratorBase
         _ = hostApplicationBuilder.Configuration
             .AddJsonFile($"appsettings.{nameof(Settings.GasStationPricesSettings)}.json", optional: false, reloadOnChange: true)
             .AddJsonFile($"appsettings.{nameof(Settings.GasStationPricesSettings)}.{CurrentEnvironmentName}.json", optional: false, reloadOnChange: true);
-        ;
     }
 
     protected override void AddDbContexts(Microsoft.Extensions.Hosting.IHostApplicationBuilder hostApplicationBuilder) { /* No DbContexts */ }
 
-    protected override void AddMyServices(Microsoft.Extensions.Hosting.IHostApplicationBuilder hostApplicationBuilder) => 
+    protected override void AddMyServices(Microsoft.Extensions.Hosting.IHostApplicationBuilder hostApplicationBuilder)
+    {
         hostApplicationBuilder.Services.TryAddScoped<Services.GasStationPricesService>();
+
+        _ = hostApplicationBuilder.Services.AddHttpClient();
+
+        //_ = hostApplicationBuilder.Services.AddHttpClient("Default")
+        //    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+        //    {
+        //        UseCookies = false
+        //    });
+
+        //_ = hostApplicationBuilder.Services.AddHttpClient("NoRedirectClient")
+        //    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+        //    {
+        //        AllowAutoRedirect = false
+        //    });
+
+        //_ = hostApplicationBuilder.Services.AddHttpClient("CookiesClient")
+        //    .ConfigurePrimaryHttpMessageHandler(() =>
+        //    {
+        //        return new HttpClientHandler
+        //        {
+        //            UseCookies = true,
+        //            CookieContainer = new System.Net.CookieContainer()
+        //        };
+        //    });
+    }
 }
